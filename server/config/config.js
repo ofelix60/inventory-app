94dd8113822310202b14bb04a60f9bbcc31dc3fa
Oscar Felix <ofelix60@gmail.com>
2022-09-12 17:02:35 -0700

    'tweak'

--- server/config/config.js
+++ server/config/config.js
@@ -3,21 +3,21 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: 'ubuntu',
-    password: null,
+    password: 'password123',
     database: 'inventory_app',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
   test: {
     username: 'ubuntu',
-    password: null,
+    password: 'password123',
     database: 'inventory_app',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
   production: {
     username: 'ubuntu',
-    password: null,
+    password: 'password123',
     database: 'inventory_app',
     host: '127.0.0.1',
     dialect: 'postgres',
