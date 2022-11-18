e3b6431e8f00356497c9749433947875074876f0
Oscar Felix <ofelix60@gmail.com>
2022-11-18 15:35:13 -0800

    Change NODE_ENV to testing.

--- server/models/index.js
+++ server/models/index.js
@@ -4,12 +4,12 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'test'; //'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-console.log(config.dialect);
+
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
